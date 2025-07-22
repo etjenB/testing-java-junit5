d64ad1ed0aed9bc204b2c9ff75e495acd7406fac
etjen <buljubasicetjen4@gmail.com>
2025-07-22 12:46:11 +0200

    parametrized tests, integration test

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import guru.springframework.sfgpetclinic.CustomArgsProvider;
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,4 +33,57 @@ class OwnerTest implements ModelTests {
         assertThat(owner.getCity(), is("Kanye West"));
     }
 
+    @DisplayName("Value Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index} {arguments}]")
+    @ValueSource(strings = {"Spring", "Framework", "Etjen"})
+    void testValueSource(String val) {
+        System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index} {arguments}]")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
+    }
+
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index} {arguments}]")
+    @CsvSource({
+            "FL, 1, 1",
+            "MI, 2, 2",
+            "OH, 3, 3"
+    })
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println("State: " + stateName + " - " + val1 + " : " + val2);
+    }
+
+    @DisplayName("CSV File Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index} {arguments}]")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFileSourceTest(String stateName, int val1, int val2) {
+        System.out.println("State: " + stateName + " - " + val1 + " : " + val2);
+    }
+
+    @DisplayName("Method Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index} {arguments}]")
+    @MethodSource("getargs")
+    void methodSourceTest(String stateName, int val1, int val2) {
+        System.out.println("State: " + stateName + " - " + val1 + " : " + val2);
+    }
+
+    static Stream<Arguments> getargs() {
+        return Stream.of(
+            Arguments.of("FL", 100, 101),
+            Arguments.of("AL", 900, 905),
+            Arguments.of("TX", 200, 201)
+        );
+    }
+
+    @DisplayName("Custom Provider Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index} {arguments}]")
+    @ArgumentsSource(CustomArgsProvider.class)
+    void customProviderSourceTest(String stateName, int val1, int val2) {
+        System.out.println("State: " + stateName + " - " + val1 + " : " + val2);
+    }
 }
